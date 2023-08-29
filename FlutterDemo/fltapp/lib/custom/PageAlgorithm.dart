6a7eb125db20a551b78e9d6c53b8d29ac3b9cd65
yanhaijun <15261882407@163.com>
2023-08-29 22:16:38 +0800

    Type:ReqDev Desc:ListView简单使用或ListView作二次扩展封装更接近业务层的LtStructure表插件 Version:

--- FlutterDemo/fltapp/lib/custom/PageAlgorithm.dart
+++ FlutterDemo/fltapp/lib/custom/PageAlgorithm.dart
@@ -60,20 +60,45 @@ class _PageAlgorithmState extends State<PageAlgorithm> {
         child: SizedBox(
           width: screenW - 20,
           height: 600,
-          child: ListView.builder(
-            scrollDirection: Axis.vertical,
-            itemBuilder: itemBuilder,
-            itemCount: cities.length,
-          ), //ListView水平滚动
+          child: LtStructure(
+            onTap: (value, index) {
+              if (kDebugMode) {
+                print(cities[index]);
+              }
+            },
+          ), //ListView作二次扩展封装更接近业务层的LtStructure表插件
         ),
       ),
     );
   }
+  // Widget build(BuildContext context) {
+  //   var physicalSize = View.of(context).physicalSize;
+  //   var devicePixelRatio = View.of(context).devicePixelRatio;
+  //   //计算出ios/android屏幕宽高 (pt/dp)
+  //   var screenW = physicalSize.width / devicePixelRatio;
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       backgroundColor: Colors.teal,
+  //       title: Text(widget.title),
+  //     ),
+  //     body: Center(
+  //       child: SizedBox(
+  //         width: screenW - 20,
+  //         height: 600,
+  //         child: ListView.builder(
+  //           scrollDirection: Axis.vertical,
+  //           itemBuilder: itemBuilder2,
+  //           itemCount: cities.length,
+  //         ), //ListView水平滚动
+  //       ),
+  //     ),
+  //   );
+  // }
 
-  Widget? itemBuilder(BuildContext context, int index) {
-    return InkWell(
+  Widget? itemBuilder0(BuildContext context, int index) {
+    return GestureDetector(
       onTap: () {
-        //点击处理
+        //点击回调处理
         if (kDebugMode) {
           print(cities[index]);
         }
@@ -85,6 +110,105 @@ class _PageAlgorithmState extends State<PageAlgorithm> {
         alignment: Alignment.center,
         decoration: const BoxDecoration(color: Colors.teal),
         child: Text(cities[index]),
+      ),
+    );
+  }
+
+  Widget? itemBuilder1(BuildContext context, int index) {
+    return InkWell(
+      onTap: () {
+        //点击回调处理
+        if (kDebugMode) {
+          print(cities[index]);
+        }
+      },
+      child: Container(
+        height: 45,
+        width: 160,
+        margin: const EdgeInsets.only(bottom: 5),
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(color: Colors.teal),
+        child: Text(cities[index]),
+      ),
+    );
+  }
+
+  Widget? itemBuilder2(BuildContext context, int index) {
+    return InkResponse(
+      onTap: () {
+        //点击回调处理
+        if (kDebugMode) {
+          print(cities[index]);
+        }
+      },
+      child: Container(
+        height: 45,
+        width: 160,
+        margin: const EdgeInsets.only(bottom: 5),
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(color: Colors.teal),
+        child: Text(cities[index]),
+      ),
+    );
+  }
+}
+
+//****通过回调函数实现逆向传值(内容数据反馈回UI左上前业务层逻辑)***
+
+//构造器类(组件)
+//(自定义)插件 集成类 > (自定义)插件 实例
+//StatelessWidget(常规型)无态SW插件>充当子层插件(上层内嵌插件)>未直接关联状态 (不依赖状态配置信息>间接内嵌渲染插件)
+class LtStructure extends StatelessWidget {
+  //自调用(Self)
+  //(自定义)待配置公开属性默认可选
+  const LtStructure({super.key, @required this.onTap});
+  //通过回调函数实现逆向传值(内容数据反馈回UI左上前业务层逻辑)
+  //void Function()? 事件本身所遵循的结构特征
+  final void Function(String, int)? onTap;
+
+  //生产编码
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      itemBuilder: (context, index) {
+        return LtItem(
+          value: cities[index],
+          onTap: (value, index) {
+            onTap!(value, index);
+          },
+          index: index,
+        );
+      },
+      itemCount: cities.length,
+    );
+  }
+}
+
+class LtItem extends StatelessWidget {
+  //自调用(Self)
+  //(自定义)待配置公开属性默认可选
+  const LtItem(
+      {super.key,
+      @required this.onTap,
+      required this.value,
+      required this.index});
+
+  final String value;
+  final int index;
+  //void Function()? 事件本身所遵循的结构特征
+  final void Function(String, int)? onTap;
+
+  //生产编码
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        //点击回调处理
+        onTap!(value, index);
+      },
+      child: ListTile(
+        title: Text(value),
       ),
     );
   }
